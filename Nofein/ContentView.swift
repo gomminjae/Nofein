61c72a0be7283fb99aa3c39614a513c6ea5711b6
gomminjae <gommj0611@naver.com>
2024-01-14 00:37:58 +0900

    Initial Commit

---
+++ Nofein/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nofein
+//
+//  Created by 권민재 on 1/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nofein/NofeinApp.swift
@@ -0,0 +1,17 @@
+//
+//  NofeinApp.swift
+//  Nofein
+//
+//  Created by 권민재 on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct NofeinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
